51808f73e951bf88a054d3c261d2b876575bdddf
AugustinSorel <sorelaugustin@gmail.com>
2023-12-26 13:33:14 +0000

    FEAT/destroy dom fn added

---
+++ packages/runtime/src/destroy-dom.ts
@@ -0,0 +1,50 @@
+import { removeEventListeners } from "./events";
+import { H, HFragment, HString, VNodes } from "./h";
+
+export const destroyDom = (vdom: VNodes) => {
+  if (vdom.type === "text") {
+    destroyTextNode(vdom);
+    return;
+  }
+
+  if (vdom.type === "element") {
+    destroyElement(vdom);
+    return;
+  }
+
+  if (vdom.type === "fragment") {
+    destroyFragmentNode(vdom);
+    return;
+  }
+
+  throw new Error(`vdom type: ${vdom} is not being handle`);
+};
+
+const destroyTextNode = (textNode: HString) => {
+  textNode.domPointer?.remove();
+
+  delete textNode.domPointer;
+};
+
+const destroyElement = (elementNode: H) => {
+  elementNode.domPointer?.remove();
+
+  for (const children of elementNode.children) {
+    destroyDom(children);
+  }
+
+  if (elementNode.listeners && elementNode.domPointer) {
+    removeEventListeners(elementNode.listeners, elementNode.domPointer);
+    delete elementNode.listeners;
+  }
+
+  delete elementNode.domPointer;
+};
+
+const destroyFragmentNode = (fragmentNode: HFragment) => {
+  for (const children of fragmentNode.children) {
+    destroyDom(children);
+  }
+
+  delete fragmentNode.domPointer;
+};
