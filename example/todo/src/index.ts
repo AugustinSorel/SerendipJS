a721d8a6b46ad99a01f75b4e22e6ace7492a634a
Augustin <48162609+AugustinSorel@users.noreply.github.com>
2024-01-01 20:29:11 +0000

    Refactor/refactor todos example with new functions (#13)
    * REFACTOR/createApp fn added
    
    * FEAT/toggle done
    
    * CHORE/fixme added

--- example/todo/src/index.ts
+++ example/todo/src/index.ts
@@ -1,61 +1,150 @@
-import { h, hFragment, hString, mountDOM } from "SerendipJS";
+import { Reducer, View, createApp, h, hFragment, hString } from "SerendipJS";
 import "./index.css";
+import { getTodos, saveTodos } from "./localStorage";
 
-const todos = [
-  { id: 1, title: "walk the dog", isDone: false },
-  { id: 2, title: "go outside", isDone: true },
-  { id: 3, title: "make food", isDone: false },
-];
+export type Todo = { id: number; title: string; isDone: boolean };
+
+const todos = getTodos();
 
 const header = h("header", { class: "main-header" }, [
   h("h1", { class: "main-title" }, [hString("todo list")]),
 ]);
 
-const newTodoForm = h("form", { class: "new-todo-form" }, [
-  h(
-    "input",
+//FIXME: create a type for emit
+const newTodoForm = (emit: any) => {
+  return h(
+    "form",
     {
-      type: "text",
-      placeholder: "new todo...",
-      class: "new-todo-input",
-    },
-    [],
-  ),
-  h("button", { class: "new-todo-submit-button", title: "add new todo" }, [
-    hString("+"),
-  ]),
-]);
+      class: "new-todo-form",
+      on: {
+        submit: (e) => {
+          e.preventDefault();
 
-const listOfTodos = h(
-  "ul",
-  { class: "list-of-todos" },
-  todos.map((todo) => {
-    return h("li", { class: "todo-item" }, [
+          //FIXME: add zod
+          const formData = new FormData(e.currentTarget);
+
+          const todoTitle = formData.get("todo-title");
+
+          emit("addNewTodo", todoTitle);
+        },
+      },
+    },
+    [
       h(
         "input",
         {
-          id: `todo-status-${todo.id}`,
-          class: "todo-item-checkbox",
-          type: "checkbox",
-          checked: todo.isDone,
+          type: "text",
+          placeholder: "new todo...",
+          class: "new-todo-input",
+          name: "todo-title",
         },
         [],
       ),
-      h("label", { class: "todo-item-title", for: `todo-status-${todo.id}` }, [
-        hString(todo.title),
+      h("button", { class: "new-todo-submit-button", title: "add new todo" }, [
+        hString("+"),
       ]),
-      h("button", { title: "edit", class: "todo-item-edit-button" }, [
-        hString("e"),
-      ]),
-      h("button", { title: "delete", class: "todo-item-delete-button" }, [
-        hString("d"),
-      ]),
-    ]);
-  }),
+    ],
+  );
+};
+
+//FIXME: create a type for emit
+const listOfTodos = (todos: Todo[], emit: any) => {
+  return h(
+    "ul",
+    { class: "list-of-todos" },
+    todos.map((todo) => {
+      return h("li", { class: "todo-item" }, [
+        h(
+          "input",
+          {
+            id: `todo-status-${todo.id}`,
+            class: "todo-item-checkbox",
+            type: "checkbox",
+            checked: todo.isDone,
+            on: {
+              //FIXME: get the correct type here
+              //FIXME: autocomple of events here
+              change: (e: any) => {
+                if (e.currentTarget.checked) {
+                  emit("markAsDone", todo.id);
+                } else {
+                  emit("removeMarkAsDone", todo.id);
+                }
+              },
+            },
+          },
+          [],
+        ),
+        h(
+          "label",
+          { class: "todo-item-title", for: `todo-status-${todo.id}` },
+          [hString(todo.title)],
+        ),
+        h("button", { title: "edit", class: "todo-item-edit-button" }, [
+          hString("e"),
+        ]),
+        h("button", { title: "delete", class: "todo-item-delete-button" }, [
+          hString("d"),
+        ]),
+      ]);
+    }),
+  );
+};
+
+const main = (todos: Todo[], emit: any) => {
+  return h("main", { class: "main-container" }, [
+    newTodoForm(emit),
+    listOfTodos(todos, emit),
+  ]);
+};
+
+const View: View<typeof todos> = (state, emit) =>
+  hFragment([header, main(state, emit)]);
+
+const reducers: Reducer<typeof todos> = {
+  //FIXME: infer the payload
+  markAsDone: (state, id: number) => {
+    const updatedState = state.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, isDone: true };
+      }
+      return todo;
+    });
+
+    saveTodos(updatedState);
+
+    return updatedState;
+  },
+
+  //FIXME: infer the payload
+  removeMarkAsDone: (state, id: number) => {
+    const updatedState = state.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, isDone: false };
+      }
+      return todo;
+    });
+
+    saveTodos(updatedState);
+
+    return updatedState;
+  },
+
+  addNewTodo: (state, newTodoTitle: string) => {
+    const newTodo: Todo = {
+      id: Math.random(),
+      isDone: false,
+      title: newTodoTitle,
+    };
+
+    const updatedState = [...state, newTodo];
+
+    saveTodos(updatedState);
+
+    return updatedState;
+  },
+};
+
+createApp({ state: todos, view: View, reducers }).mount(
+  document.getElementById("app")!,
 );
-
-const main = h("main", { class: "main-container" }, [newTodoForm, listOfTodos]);
-
-const app = hFragment([header, main]);
-
-mountDOM(app, document.getElementById("app")!);
---
+++ example/todo/src/localStorage.ts
@@ -0,0 +1,17 @@
+//FIXME: add zod
+
+import { Todo } from ".";
+
+const localStorageKey = "todo";
+
+export const saveTodos = (todos: Todo[]) => {
+  localStorage.setItem(localStorageKey, JSON.stringify(todos));
+};
+
+export const getTodos = () => {
+  const todos = localStorage.getItem(localStorageKey) ?? "";
+
+  const parsedTodos = JSON.parse(todos) || [];
+
+  return parsedTodos as Todo[];
+};
