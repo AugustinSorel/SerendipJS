89fac49c66e9775604e3d1b03f3e73f74e340452
AugustinSorel <sorelaugustin@gmail.com>
2024-01-01 17:57:09 +0000

    REFACTOR/createApp fn added

--- example/todo/src/index.ts
+++ example/todo/src/index.ts
@@ -1,7 +1,9 @@
-import { h, hFragment, hString, mountDOM } from "SerendipJS";
+import { View, createApp, h, hFragment, hString } from "SerendipJS";
 import "./index.css";
 
-const todos = [
+type Todo = { id: number; title: string; isDone: boolean };
+
+const todos: Todo[] = [
   { id: 1, title: "walk the dog", isDone: false },
   { id: 2, title: "go outside", isDone: true },
   { id: 3, title: "make food", isDone: false },
@@ -26,36 +28,47 @@ const newTodoForm = h("form", { class: "new-todo-form" }, [
   ]),
 ]);
 
-const listOfTodos = h(
-  "ul",
-  { class: "list-of-todos" },
-  todos.map((todo) => {
-    return h("li", { class: "todo-item" }, [
-      h(
-        "input",
-        {
-          id: `todo-status-${todo.id}`,
-          class: "todo-item-checkbox",
-          type: "checkbox",
-          checked: todo.isDone,
-        },
-        [],
-      ),
-      h("label", { class: "todo-item-title", for: `todo-status-${todo.id}` }, [
-        hString(todo.title),
-      ]),
-      h("button", { title: "edit", class: "todo-item-edit-button" }, [
-        hString("e"),
-      ]),
-      h("button", { title: "delete", class: "todo-item-delete-button" }, [
-        hString("d"),
-      ]),
-    ]);
-  }),
+const listOfTodos = (todos: Todo[]) => {
+  return h(
+    "ul",
+    { class: "list-of-todos" },
+    todos.map((todo) => {
+      return h("li", { class: "todo-item" }, [
+        h(
+          "input",
+          {
+            id: `todo-status-${todo.id}`,
+            class: "todo-item-checkbox",
+            type: "checkbox",
+            checked: todo.isDone,
+          },
+          [],
+        ),
+        h(
+          "label",
+          { class: "todo-item-title", for: `todo-status-${todo.id}` },
+          [hString(todo.title)],
+        ),
+        h("button", { title: "edit", class: "todo-item-edit-button" }, [
+          hString("e"),
+        ]),
+        h("button", { title: "delete", class: "todo-item-delete-button" }, [
+          hString("d"),
+        ]),
+      ]);
+    }),
+  );
+};
+
+const main = (todos: Todo[]) => {
+  return h("main", { class: "main-container" }, [
+    newTodoForm,
+    listOfTodos(todos),
+  ]);
+};
+
+const View: View<typeof todos> = (state) => hFragment([header, main(state)]);
+
+createApp({ state: todos, view: View, reducers: {} }).mount(
+  document.getElementById("app")!,
 );
-
-const main = h("main", { class: "main-container" }, [newTodoForm, listOfTodos]);
-
-const app = hFragment([header, main]);
-
-mountDOM(app, document.getElementById("app")!);
