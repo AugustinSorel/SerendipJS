51e033583a374a2ecbc8ea1eab24af27f6d18921
Augustin <48162609+AugustinSorel@users.noreply.github.com>
2024-01-05 20:10:33 +0000

    Feat/emit type (#17)
    * FEAT/emit type added
    
    * REFACTOR/todo example refactored
    
    * REFACTOR/coutner example refactored

--- example/counter/src/index.ts
+++ example/counter/src/index.ts
@@ -1,16 +1,16 @@
-import { createApp, h, hString, Reducer, View } from "SerendipJS";
+import { createApp, h, hString, Reducers, View } from "SerendipJS";
 import "./index.css";
 
 const state = { count: 0 };
 
-const View: View<typeof state> = (state, emit) => {
+const View: View<typeof state, typeof reducers> = (state, emit) => {
   return h("main", { class: "main-container" }, [
     h(
       "button",
       {
         class: "btn text-massive tooltip",
         "data-tooltip": "decrement",
-        on: { click: () => emit("sub") },
+        on: { click: () => emit("sub", undefined) },
       },
       [hString("-")],
     ),
@@ -22,17 +22,17 @@ const View: View<typeof state> = (state, emit) => {
       {
         class: "btn text-massive tooltip",
         "data-tooltip": "increment",
-        on: { click: () => emit("add", 100) },
+        on: { click: () => emit("add", undefined) },
       },
       [hString("+")],
     ),
   ]);
 };
 
-const reducers: Reducer<typeof state> = {
+const reducers = {
   add: (state) => ({ count: state.count + 1 }),
   sub: (state) => ({ count: state.count - 1 }),
-};
+} satisfies Reducers<typeof state>;
 
 createApp({ state, view: View, reducers }).mount(
   document.getElementById("app")!,
