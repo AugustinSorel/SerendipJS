60fe1c4dbaed608f281b20e267dadecb53f35127
AugustinSorel <sorelaugustin@gmail.com>
2024-01-07 11:45:34 +0000

    FEAT/alert and modal dialog added in todo example

--- example/todo/src/todo.ts
+++ example/todo/src/todo.ts
@@ -1,4 +1,13 @@
-import { Todo, todosSchema } from ".";
+import { z } from "zod";
+
+export const todoSchema = z.object({
+  id: z.number(),
+  title: z.string().trim().min(1),
+  isDone: z.boolean(),
+});
+export const todosSchema = todoSchema.array();
+
+export type Todo = z.infer<typeof todoSchema>;
 
 const localStorageKey = "todo";
 
@@ -6,7 +15,7 @@ export const saveTodos = (todos: Todo[]) => {
   localStorage.setItem(localStorageKey, JSON.stringify(todos));
 };
 
-export const getTodos = () => {
+export const getTodos = (): Todo[] => {
   try {
     const unsafeTodos = localStorage.getItem(localStorageKey) ?? "";
     const todos = todosSchema.parse(JSON.parse(unsafeTodos));
